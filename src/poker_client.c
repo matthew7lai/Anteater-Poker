78d3ffa92ee046653f361d54e8cde15edcd8e35d
matthew7lai <matthew7lai@gmail.com>
2026-06-08 00:42:06 -0700

    fixing gtk

--- src/poker_client.c
+++ src/poker_client.c
@@ -670,7 +670,7 @@ static void on_activate(GtkApplication *app, gpointer data) {
 
 int main(int argc, char **argv) {
     GtkApplication *app=gtk_application_new("edu.uci.eecs22l.poker_client",
-                                             G_APPLICATION_DEFAULT_FLAGS);
+                                             G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app,"activate",G_CALLBACK(on_activate),NULL);
     int s=g_application_run(G_APPLICATION(app),argc,argv);
     g_object_unref(app);
